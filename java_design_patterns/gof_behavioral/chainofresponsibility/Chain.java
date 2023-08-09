c98fc2576b1f6b28034aaaf8dab35728ce01a19f
trung <trung150502@gmail.com>
2023-08-09 21:29:36 +0700

    Combine into 1 file in Chain Responsibility

--- java_design_patterns/gof_behavioral/chainofresponsibility/Chain.java
+++
@@ -1,6 +0,0 @@
-package java_design_patterns.gof_behavioral.chainofresponsibility;
-
-interface Chain {
-    void setNext(Chain nextInChain);
-    void process(Number request);
-}
--- java_design_patterns/gof_behavioral/chainofresponsibility/ChainDemo.java
+++ java_design_patterns/gof_behavioral/chainofresponsibility/ChainDemo.java
@@ -1,7 +1,43 @@
-package java_design_patterns.gof_behavioral.chainofresponsibility;
+// package java_design_patterns.gof_behavioral.chainofresponsibility;
+
+abstract class Chain {
+    protected Chain nextChain;
+
+    public void setNext(Chain nextChain) {
+        this.nextChain = nextChain;
+    }
+
+    abstract public String process(int number);
+}
+
+class ZeroProcessor extends Chain {
+    public String process(int number) {
+        if(number == 0)
+            return "ZeroProcessor : " + number;
+            
+        return this.nextChain.process(number);
+    }
+}
+
+class NegativeProcessor extends Chain {
+    public String process(int number) {
+        if(number < 0)
+            return "NegativeProcessor : " + number;
+            
+        return this.nextChain.process(number);
+    }
+}
+
+class PositiveProcessor extends Chain {
+    public String process(int number) {
+        if(number > 0)
+            return "PositiveProcessor : " + number;
+            
+        return this.nextChain.process(number);
+    }
+}
 
 public class ChainDemo {
-
     public static void main(String[] args) {
 
         //Configure Chain of Responsibility
@@ -13,10 +49,9 @@ public class ChainDemo {
         c2.setNext(c3);
 
         //Calling Chain of Responsibility
-        c1.process(new Number(40));
-        c1.process(new Number(-10));
-        c1.process(new Number(0));
-        c1.process(new Number(32));
-
+        System.out.println( c1.process(68) );
+        System.out.println( c1.process(-86) );
+        c1.process(0);
+        c1.process(156);
     }
 }
